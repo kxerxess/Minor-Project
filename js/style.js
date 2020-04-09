eb5f93c48dafc2b57e26aad83815f541f61fe6ea
kxerxess <sharma.kaustubh99@gmail.com>
2020-04-09 12:52:58 +0530

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $(".overlay").delay(1000).fadeOut(500);
+
+  var screenHeight = $(document).height();
+  var screenWidth = $(document).width();
+  console.log("Width",screenWidth);
+  console.log("Height",screenHeight);
+
+console.log("ready");
+$(".main-text").delay(1000).fadeIn(2000);
+
+});
